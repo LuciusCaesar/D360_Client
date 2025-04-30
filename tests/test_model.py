00b3e16138ad55fd321a29ba50ecde4e161fdc31
Lucius Caesar <Lucien.Culot@gmail.com>
2025-04-30 15:57:20 +0200

    Use factories for testing + remove useless testing (hello-world test)

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,61 +1,23 @@
-from data360.model import AssetClass, AssetClassName, AssetType
+from tests.model_factory import AssetTypeFactory
 
 
 def test_asset_types_are_equals_when_their_name_is_equal():
-    type1 = AssetType(
-        id="1234567890",
-        uid="1234567890",
-        name="Test Asset Type",
-        asset_class=AssetClass(
-            id="0987654321",
-            value="Test Asset Class",
-            name=AssetClassName.BUSINESS_ASSET,
-            description="Test Asset Class Description",
-            allow_comments_on_asset=True,
-        ),
-        description="Test Asset Type Description",
-        auto_display_description=True,
-        hierarchical=False,
-        hierarchy_maximum_depth=0,
-        display_format="Test Display Format",
-        notes="Test Notes",
-        use_as_transformation=False,
-        can_own_fusion=False,
-        path="Test Path",
-        can_edit_parent=False,
-        is_description_enabled=True,
-        is_description_visible_by_default=True,
-        is_default_read_access_enabled=True,
-    )
-
-    type2 = AssetType(
-        id="40594",
-        uid="52600",
-        name="Test Asset Type",
-        asset_class=AssetClass(
-            id="0987654321",
-            value="Test Asset Class",
-            name=AssetClassName.BUSINESS_ASSET,
-            description="Test Asset Class Description",
-            allow_comments_on_asset=True,
-        ),
-        description="Test Asset Type Description",
-        auto_display_description=True,
-        hierarchical=False,
-        hierarchy_maximum_depth=0,
-        display_format="Test Display Format",
-        notes="Test Notes",
-        use_as_transformation=False,
-        can_own_fusion=False,
-        path="Test Path",
-        can_edit_parent=False,
-        is_description_enabled=True,
-        is_description_visible_by_default=True,
-        is_default_read_access_enabled=True,
-    )
+    type1 = AssetTypeFactory.build(name="Asset Type Name")
+    type2 = AssetTypeFactory.build(name="Asset Type Name")
 
     assert type1 == type2
     assert type1.__eq__(type2)
     assert {type1} == {type2}
     assert [type1] == [type2]
     assert set([type1]) == set([type2])
+
+
+def test_asset_types_are_not_equals_when_their_name_is_not_equal():
+    type1 = AssetTypeFactory.build(name="Asset Type Name A")
+    type2 = AssetTypeFactory.build(name="Asset Type Name B")
+
+    assert not type1 == type2
+    assert not type1.__eq__(type2)
+    assert not {type1} == {type2}
+    assert not [type1] == [type2]
+    assert not set([type1]) == set([type2])
